bc097785afcd19129a9e51b8294a32e43b78d8b8
Kyrill <kyrill.spiegl@gmail.com>
2019-09-02 14:56:24 +0900

    no need for downcasting just declare all methods in every class

--- Circle.java
+++ Circle.java
@@ -5,10 +5,16 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Circle
+public class Circle implements Shape
 {
     public int radius;
     public Circle(int r){
         this.radius = r;
+    }
+    public double getArea(){
+        // hey
+    }
+    public void draw(){
+        // hey
     }
 }
